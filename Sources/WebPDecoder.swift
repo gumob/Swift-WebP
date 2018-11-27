795b1829e9a54edc3d28849e4bf99c6885ca707c
gumob <account@gumob.com>
2018-11-27 21:10:47 +0900

    Update WebPDecoder as a throwable function

--- Sources/WebPDecoder.swift
+++ Sources/WebPDecoder.swift
@@ -7,68 +7,78 @@ import Foundation
 import CoreGraphics
 import CWebP
 
+enum WebPDecodeError: Int, Error {
+    case ok = 0
+    case configFailure         // configuration failure
+    case brokenHeader          // broken header
+    case decodeFailure         // decode failure
+}
+
 public struct WebPDecoder {
 
-//    public static func decode(_ webPData: Data) throws -> CGImage {
-//        var config: CWebP.WebPDecoderConfig = webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-//            var config = CWebP.WebPDecoderConfig()
-//            if WebPInitDecoderConfig(&config) == 0 {
-//                fatalError("can't init decoder config")
-//            }
-//
-//            var features = CWebP.WebPBitstreamFeatures()
-//            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK {
-//                fatalError("broken header")
-//            }
-//
-//            config.output.colorspace = MODE_RGBA
-//
-//            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK {
-//                fatalError("failure decode")
-//            }
-//            return config
-//        }
-//
-//        let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        let provider = CGDataProvider(dataInfo: &config,
-//                                      data: config.output.u.RGBA.rgba,
-//                                      size: (Int(config.input.width) * Int(config.input.height) * 4),
-//                                      releaseData: webp_freeWebPData)!
-//        let cgImage = CGImage(
-//                width: Int(config.input.width),
-//                height: Int(config.input.height),
-//                bitsPerComponent: 8,
-//                bitsPerPixel: 32,
-//                bytesPerRow: Int(config.output.u.RGBA.stride),
-//                space: colorSpace,
-//                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue),
-//                provider: provider,
-//                decode: nil,
-//                shouldInterpolate: false,
-//                intent: CGColorRenderingIntent.defaultIntent)!
-//
-//        return cgImage
-//    }
+    public static func decode(_ webPData: Data) throws -> CGImage {
+        var config: CWebP.WebPDecoderConfig = try webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+            var config = CWebP.WebPDecoderConfig()
+            if WebPInitDecoderConfig(&config) == 0 {
+                throw WebPDecodeError.configFailure
+            }
 
-    public static func decode(_ webPData: Data, checkStatus: Bool = false) -> (Int, Int) {
-
-        let config: WebPDecoderConfig = webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-            var config = WebPDecoderConfig()
-            if WebPInitDecoderConfig(&config) == 0 { return config }
-
-            var features = WebPBitstreamFeatures()
-            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK && checkStatus { return config }
+            var features = CWebP.WebPBitstreamFeatures()
+            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK {
+                throw WebPDecodeError.brokenHeader
+            }
 
             config.output.colorspace = MODE_RGBA
 
-            /* If chunk webPData is passed to decoder it always returns fault status.
-             * Do not check status in this case. */
-            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK && checkStatus  { return config }
-//            WebPDecode(body, webPData.count, &config)
+            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK {
+                throw WebPDecodeError.decodeFailure
+            }
+            return config
+        }
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let provider = CGDataProvider(dataInfo: &config,
+                                      data: config.output.u.RGBA.rgba,
+                                      size: (Int(config.input.width) * Int(config.input.height) * 4),
+                                      releaseData: webp_freeWebPData)!
+        let cgImage = CGImage(
+                width: Int(config.input.width),
+                height: Int(config.input.height),
+                bitsPerComponent: 8,
+                bitsPerPixel: 32,
+                bytesPerRow: Int(config.output.u.RGBA.stride),
+                space: colorSpace,
+                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue),
+                provider: provider,
+                decode: nil,
+                shouldInterpolate: false,
+                intent: CGColorRenderingIntent.defaultIntent)!
+
+        return cgImage
+    }
+
+    public static func decode(_ webPData: Data, checkStatus: Bool = false) throws -> CGSize {
+
+        let config: WebPDecoderConfig = try webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+            var config = WebPDecoderConfig()
+            if WebPInitDecoderConfig(&config) == 0 {
+                throw WebPDecodeError.configFailure
+            }
+
+            var features = WebPBitstreamFeatures()
+            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK && checkStatus {
+                throw WebPDecodeError.brokenHeader
+            }
+
+            config.output.colorspace = MODE_RGBA
+
+            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK && checkStatus {
+                throw WebPDecodeError.decodeFailure
+            }
 
             return config
         }
 
-        return (Int(config.input.width), Int(config.input.height))
+        return CGSize(width: Int(config.input.width), height: Int(config.input.height))
     }
 }
--- Tests/WebPTests/WebPDecoderTests.swift
+++ Tests/WebPTests/WebPDecoderTests.swift
@@ -20,44 +20,40 @@ class WebPDecoderTests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testVP8() {
         let url = Bundle(for: self.classForCoder).url(forResource: "jiro-vp8", withExtension: "webp")!
         let data = try! Data(contentsOf: url)
-        let size = WebPDecoder.decode(data, checkStatus: true)
-        XCTAssertEqual(size.0, 1210)
-        XCTAssertEqual(size.1, 907)
+        let size = try! WebPDecoder.decode(data, checkStatus: true)
+        XCTAssertEqual(size, CGSize(width: 1210, height: 907))
     }
-    
+
     func testVP8L() {
         let url = Bundle(for: self.classForCoder).url(forResource: "jiro-vp8l", withExtension: "webp")!
         let data = try! Data(contentsOf: url)
-        let size = WebPDecoder.decode(data, checkStatus: true)
-        XCTAssertEqual(size.0, 1210)
-        XCTAssertEqual(size.1, 907)
+        let size = try! WebPDecoder.decode(data, checkStatus: true)
+        XCTAssertEqual(size, CGSize(width: 1210, height: 907))
     }
-    
+
     func testVP8Header() {
         let url = Bundle(for: self.classForCoder).url(forResource: "jiro-vp8-only-header", withExtension: "webp")!
         let data = try! Data(contentsOf: url)
-        let size = WebPDecoder.decode(data, checkStatus: false)
-        XCTAssertEqual(size.0, 1210)
-        XCTAssertEqual(size.1, 907)
+        let size = try! WebPDecoder.decode(data, checkStatus: true)
+        XCTAssertEqual(size, CGSize(width: 1210, height: 907))
     }
-    
+
     func testVP8LHeader() {
         let url = Bundle(for: self.classForCoder).url(forResource: "jiro-vp8l-only-header", withExtension: "webp")!
         let data = try! Data(contentsOf: url)
-        let size = WebPDecoder.decode(data, checkStatus: false)
-        XCTAssertEqual(size.0, 1210)
-        XCTAssertEqual(size.1, 907)
+        let size = try! WebPDecoder.decode(data, checkStatus: true)
+        XCTAssertEqual(size, CGSize(width: 1210, height: 907))
     }
-    
+
     /* TODO: Add a test case for VP8X */
 //    func testVP8X() {
 //        let url = Bundle(for: self.classForCoder).url(forResource: "jiro-vp8x", withExtension: "webp")!
