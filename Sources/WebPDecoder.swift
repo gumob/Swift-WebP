198ebae78289c19be1e98b29563904cbe9e51c65
gumob <account@gumob.com>
2018-11-27 17:11:45 +0900

    Temporary disable default decode function

--- Sources/WebPDecoder.swift
+++ Sources/WebPDecoder.swift
@@ -9,46 +9,46 @@ import CWebP
 
 public struct WebPDecoder {
 
-    public static func decode(_ webPData: Data) throws -> CGImage {
-        var config: CWebP.WebPDecoderConfig = webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-            var config = CWebP.WebPDecoderConfig()
-            if WebPInitDecoderConfig(&config) == 0 {
-                fatalError("can't init decoder config")
-            }
-
-            var features = CWebP.WebPBitstreamFeatures()
-            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK {
-                fatalError("broken header")
-            }
-
-            config.output.colorspace = MODE_RGBA
-
-            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK {
-                fatalError("failure decode")
-            }
-            return config
-        }
-
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let provider = CGDataProvider(dataInfo: &config,
-                                      data: config.output.u.RGBA.rgba,
-                                      size: (Int(config.input.width) * Int(config.input.height) * 4),
-                                      releaseData: webp_freeWebPData)!
-        let cgImage = CGImage(
-                width: Int(config.input.width),
-                height: Int(config.input.height),
-                bitsPerComponent: 8,
-                bitsPerPixel: 32,
-                bytesPerRow: Int(config.output.u.RGBA.stride),
-                space: colorSpace,
-                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue),
-                provider: provider,
-                decode: nil,
-                shouldInterpolate: false,
-                intent: CGColorRenderingIntent.defaultIntent)!
-
-        return cgImage
-    }
+//    public static func decode(_ webPData: Data) throws -> CGImage {
+//        var config: CWebP.WebPDecoderConfig = webPData.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+//            var config = CWebP.WebPDecoderConfig()
+//            if WebPInitDecoderConfig(&config) == 0 {
+//                fatalError("can't init decoder config")
+//            }
+//
+//            var features = CWebP.WebPBitstreamFeatures()
+//            if WebPGetFeatures(body, webPData.count, &features) != VP8_STATUS_OK {
+//                fatalError("broken header")
+//            }
+//
+//            config.output.colorspace = MODE_RGBA
+//
+//            if WebPDecode(body, webPData.count, &config) != VP8_STATUS_OK {
+//                fatalError("failure decode")
+//            }
+//            return config
+//        }
+//
+//        let colorSpace = CGColorSpaceCreateDeviceRGB()
+//        let provider = CGDataProvider(dataInfo: &config,
+//                                      data: config.output.u.RGBA.rgba,
+//                                      size: (Int(config.input.width) * Int(config.input.height) * 4),
+//                                      releaseData: webp_freeWebPData)!
+//        let cgImage = CGImage(
+//                width: Int(config.input.width),
+//                height: Int(config.input.height),
+//                bitsPerComponent: 8,
+//                bitsPerPixel: 32,
+//                bytesPerRow: Int(config.output.u.RGBA.stride),
+//                space: colorSpace,
+//                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue),
+//                provider: provider,
+//                decode: nil,
+//                shouldInterpolate: false,
+//                intent: CGColorRenderingIntent.defaultIntent)!
+//
+//        return cgImage
+//    }
 
     static func decode(_ webPData: Data, checkStatus: Bool = false) -> (Int, Int) {
 
